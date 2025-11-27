8efe94dd05eb57fe5956a1190851c396c59df674
MohammedAliBelay <eng.mame17@gmail.com>
2025-11-27 19:04:13 +0300

    banner updated

--- netflix-clone/src/components/Banner/Banner.jsx
+++ netflix-clone/src/components/Banner/Banner.jsx
@@ -1,19 +1,16 @@
-import  { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "../../utils/axios/Axios.jsx";
 import requests from "../../utils/request/Request.jsx";
 import "./banner.css";
 
 function Banner() {
-  const [movie, setMovie] = useState(null);
+  const [movie, setMovie] = useState({});
 
   useEffect(() => {
     async function fetchData() {
-      const request = await axios.get(requests.fetchTrending);
-      setMovie(
-        request.data.results[
-          Math.floor(Math.random() * request.data.results.length)
-        ]
-      );
+      const request = await axios.get(requests.fetchTopRated);
+      const results = request.data.results;
+      setMovie(results[Math.floor(Math.random() * results.length)]);
     }
     fetchData();
   }, []);
--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
-// import Banner from "../../components/Banner/Banner";
+import Banner from "../../components/Banner/Banner";
 // import Row from "../../components/Row/Row";
 
 const Home = () => {
   return (
     <>
       <Header />
-      {/* <Banner />
-      <Row /> */}
+      <Banner />
+      {/* <Row /> */}
 
       <Footer />
     </>
